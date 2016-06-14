795a5b3417bd939c5be38c0949b680fe711950ea
钩吻 <pinian.lpn@alibaba-inc.com>
2016-06-14 19:16:33 +0800

    Merge branch 'sharding-jdbc-1.x'

--- mvn-repository/github-deply.bat
+++ mvn-repository/github-deply.bat
@@ -3,7 +3,4 @@ del com/
 cd ../sjdbc/
 call mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
 
-cd ../sjdbc-tx/
-call mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
-
 @pause
--- mvn-repository/github-deply.sh
+++ mvn-repository/github-deply.sh
@@ -4,6 +4,3 @@ rm -rf com/
 
 cd ../sjdbc/
 mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
-
-cd ../sjdbc-tx/
-mvn -DaltDeploymentRepository=snapshot-repo::default::file:../mvn-repository clean deploy
